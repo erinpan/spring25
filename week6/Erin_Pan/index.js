fdd1015844351ced9994f3fcea085e9d2f53fad7
erinpan <erin.j.pan1234@gmail.com>
2025-03-13 20:23:08 -0700

    updated chat with nickname

--- week6/Erin_Pan/index.js
+++ week6/Erin_Pan/index.js
@@ -1,18 +1,30 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const express = require("express");
+const http = require("http");
 const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("chat message", (data) => {
+        io.emit("chat message", { 
+            nickname: data.nickname, 
+            message: data.message 
+        });
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log("Server running on http://localhost:3000");
 });
